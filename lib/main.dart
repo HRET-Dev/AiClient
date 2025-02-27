4178e254e09d2ae1c5d6335b10cebb0f5a2d2a5c
HRET <2126366332@qq.com>
2025-02-27 22:15:57 +0800

    feat(database): 添加 多平台 SQLite 数据库支持
    - 在 main 函数中添加了对 SQLite 数据库的初始化和配置
    - 引入了 sqflite_common_ffi 和 sqflite_common_ffi_web 包- 在 pubspec.yaml 中添加了相关依赖
    - 在 assets 中添加了 SQLite3 的 DLL 文件

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
 
 import 'app.dart';
 
 void main() async {
+  if (kIsWeb) {
+    // Web 平台使用 sqfLite_common_ffi_web 的工厂
+    databaseFactory = databaseFactoryFfiWeb;
+  } else {
+    // 显式初始化 SqlLite FFI 环境
+    sqfliteFfiInit();
+    // 初始化 FFI 数据库工厂
+    databaseFactory = databaseFactoryFfi;
+  }
+
   // 初始化Easy_Localization库
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
